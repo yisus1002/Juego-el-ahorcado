e8e26a7f7a2f14305f25ec3a47e75fb0e349c125
Jesus Garcia <yisusgarcia1002@gmail.com>
2021-12-03 10:34:56 -0500

    se ingreso una palabra que el usuario va a adivinar y se creo un metodo que recorra el tamaño de la palabra oculta y muestre '_ ' por cada letra

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ahorcado';
 
+  palabra='AGUACATE';
+  palabraOculta='';
+
 
   letras=['A','B','C','D','E','F','G','H','I', 'J',
           'K','L','M', 'N','Ñ','O','P','Q','R','S',
           'T','U','V','W','X','Y','Z'];
 
   constructor(){
-    console.log('se creo la app');
+    // console.log('se creo la app');
+    // por cada letra que tenga la palabra se va agregar un '_ '
+    this.palabraOculta ='_ '.repeat(this.palabra.length);
   }
 }
