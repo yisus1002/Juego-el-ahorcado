9b2b85863988a407b02ed59d96c2cc65ecf0f670
Jesus Garcia <yisusgarcia1002@gmail.com>
2021-12-03 10:25:39 -0500

    Agregue los botones con las letras del A-Z con un for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ahorcado';
+
+
+  letras=['A','B','C','D','E','F','G','H','I', 'J',
+          'K','L','M', 'N','Ñ','O','P','Q','R','S',
+          'T','U','V','W','X','Y','Z'];
+
+  constructor(){
+    console.log('se creo la app');
+  }
 }
